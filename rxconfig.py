3a4080f14b4801f4bc09c48f57745f88c41ce287
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 12:32:57 -0400

    prueba 10

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,6 @@ config = rx.Config(
     app_name="app",
     backend_host="0.0.0.0",
     backend_port=8000,
-    api_url="https://rf-agenda-de-citas-production.up.railway.app:8000",
+    frontend_port=3000,
+    api_url="https://rf-agenda-de-citas-production.up.railway.app:8000/_event",
 )
