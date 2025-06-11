ba9056219667f906e0567f1f361b8e0d60274f67
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 11:29:33 -0400

    prueba 1

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,9 @@ import reflex as rx
 import os
 
 config = rx.Config(
-    app_name="app"
+    app_name="app",
+    backend_host="0.0.0.0",
+    backend_port=8000,
+    frontend_port=3001,
+    api_url=f"http://0.0.0.0:8000" if os.getenv("RAILWAY_ENVIRONMENT") else None,
 )
