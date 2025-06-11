602062337626a0b290dd9f5187317be5cce0cb69
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 12:50:39 -0400

    prueba 12

--- rxconfig.py
+++ rxconfig.py
@@ -5,7 +5,7 @@ config = rx.Config(
     app_name="app",
     backend_host="0.0.0.0",
     backend_port=int(os.getenv("PORT", 8000)),
-    frontend_port=3001,
+    frontend_port=3000,
     # Forzar que use la URL del dominio público
     api_url=os.getenv("RAILWAY_STATIC_URL") or os.getenv("RAILWAY_PUBLIC_DOMAIN") or None,
 )
