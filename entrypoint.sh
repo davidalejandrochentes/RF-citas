ca048975d450077d931ab85c043c0fe62b1c35f0
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 11:57:52 -0400

    prueba 6

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Starting Reflex app in production mode..."
-reflex run --env prod --backend-port 8000 --frontend-port 3000
+reflex run
--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,4 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
-    backend_port=8000,
-    frontend_port=3000,
 )
