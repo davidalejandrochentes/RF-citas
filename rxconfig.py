10c09c586bd6aa6f3f2cdfff95b7fec84f314f1d
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 12:26:58 -0400

    prueba 9

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="app",
+    backend_host="0.0.0.0",
+    backend_port=8000,
     api_url="https://rf-agenda-de-citas-production.up.railway.app:8000",
 )
