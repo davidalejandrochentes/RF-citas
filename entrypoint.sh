8af8f51df802faf00fdd65a3ff20e9aaab14a60c
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 15:06:45 -0400

    inicio

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-
 echo "Starting Reflex app in production mode..."
-
-export PORT=${PORT:-8000}
-
-echo "Running on single port: $PORT"
-
-# Ejecutar sin --frontend-port para que use el mismo puerto
-reflex run --env prod --backend-host 0.0.0.0 --backend-port $PORT
+reflex run --env prod
--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,7 @@
 import reflex as rx
-import os
 
 config = rx.Config(
     app_name="app",
-    backend_host="0.0.0.0", 
-    backend_port=int(os.getenv("PORT", 8000)),
-    # NO especificar frontend_port para que use el mismo puerto del backend
+    backend_port=8000,
+    frontend_port=3000
 )
