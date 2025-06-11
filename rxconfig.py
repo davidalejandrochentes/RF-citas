3fe45a22ecfa9b1a3c34ba23e3508806d3551370
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 12:44:42 -0400

    prueba 11

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,11 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="app",
     backend_host="0.0.0.0",
-    backend_port=8000,
-    frontend_port=3000,
-    api_url="https://rf-agenda-de-citas-production.up.railway.app:8000/_event",
+    backend_port=int(os.getenv("PORT", 8000)),
+    frontend_port=3001,
+    # Forzar que use la URL del dominio público
+    api_url=os.getenv("RAILWAY_STATIC_URL") or os.getenv("RAILWAY_PUBLIC_DOMAIN") or None,
 )
