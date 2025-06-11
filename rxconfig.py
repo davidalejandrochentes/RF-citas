6efa19d9ca9c05c0c3c8d7340789de30d9058f2e
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 11:46:49 -0400

    prueba 3

--- rxconfig.py
+++ rxconfig.py
@@ -1,15 +1,10 @@
 import reflex as rx
-import os
-
-# Railway proporciona estas variables de entorno
-railway_url = os.getenv("RAILWAY_PUBLIC_DOMAIN")
-port = int(os.getenv("PORT", 8000))
 
 config = rx.Config(
     app_name="app",
-    backend_host="0.0.0.0",
-    backend_port=port,
-    frontend_port=3001,
+    backend_host="https://rf-agenda-de-citas-production.up.railway.app",
+    backend_port=8000,
+    frontend_port=3000,
     # Usar la URL pública de Railway para la API
-    api_url=f"https://{railway_url}" if railway_url else f"http://0.0.0.0:{port}",
+    #api_url=f"https://rf-agenda-de-citas-production.up.railway.app/8000",
 )
