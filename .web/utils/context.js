3a8862af25b5bfb97cc3558f75f87605ab7566a0
david alejandro chentes <davidalejandrochentes@gmail.com>
2025-06-11 18:22:46 -0400

    quitar la marca

--- .web/utils/context.js
+++ .web/utils/context.js
@@ -53,7 +53,7 @@ export const initialEvents = () => [
 
 export const isDevMode = true
 
-export const lastCompiledTimeStamp = "2025-06-11 17:57:45.522273"
+export const lastCompiledTimeStamp = "2025-06-11 18:22:33.166219"
 
 export function UploadFilesProvider({ children }) {
   const [filesById, setFilesById] = useState({})
--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,5 @@ import os
 
 config = rx.Config(
     app_name="app",
+    show_reflex_badge=False,
 )
